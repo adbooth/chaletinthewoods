e5bdbb011b7085b5758fe78e6bed6d39c07790a4
Andrew Booth <adbooth8@gmail.com>
2016-03-25 01:23:55 -0400

    First commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+/* js/script.js */
+function put_scene(elem){
+    // Remove previous active scene
+    $('.nav-entry').removeClass('active');
+    $('.scene').addClass('hidden');
+
+    // Get name of scene
+    var name = elem.id.split('_')[0];
+    // Get corresponding nav entry and scene
+    var nav_entry = $('#' + name + '_nav-entry');
+    var scene = $('#' + name + '_scene');
+    // Make scene active
+    nav_entry.addClass('active');
+    scene.removeClass('hidden');
+}
+
+function put_contact(){
+    put_scene(document.getElementById('Contact_scene'));
+}
